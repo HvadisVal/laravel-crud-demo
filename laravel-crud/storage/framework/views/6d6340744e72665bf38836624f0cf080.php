2225082d0857157f95ff6c88672fce72406bcfeb
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2025-02-19 23:53:36 +0100

    last updates

---
+++ laravel-crud/storage/framework/views/6d6340744e72665bf38836624f0cf080.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 8 CRUD Application</title>
+</head>
+<body>
+
+    <h1>  Laravel 8 CRUD Application </h1>
+    <div class="container">
+        <br>
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+    
+</body>
+</html><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/de-crud/laravel-crud/resources/views/products/layout.blade.php ENDPATH**/ ?>
